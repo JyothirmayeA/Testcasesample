a59cfe1db67db1728709d571908476835563f2d0
424701 <424701@LINL190403110.next.loc>
2019-06-26 13:37:34 +0530

    First Commit

---
+++ SampleDemoMaven/src/test/java/Mavenproject/testcase1Test.java
@@ -0,0 +1,11 @@
+package Mavenproject;
+
+import org.testng.annotations.Test;
+
+public class testcase1Test{
+@Test
+	public void Demo() {
+		
+		System.out.println("hi");
+	}
+}
---
+++ SampleDemoMaven/src/test/java/Mavenproject/testcase2Test.java
@@ -0,0 +1,11 @@
+package Mavenproject;
+
+import org.testng.annotations.Test;
+
+public class testcase2Test {
+	@Test
+	public void case2() {
+		System.out.println("case2");
+	}
+
+}
